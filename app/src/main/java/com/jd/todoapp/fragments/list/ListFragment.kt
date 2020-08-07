68239e4ff9e8a8d6eb13b2403f64dfe351b1fb18
JD <juandiegogarita@gmail.com>
2020-08-06 21:31:51 -0600

    includes menu for list fragment

--- app/src/main/java/com/jd/todoapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/jd/todoapp/fragments/list/ListFragment.kt
@@ -1,9 +1,7 @@
 package com.jd.todoapp.fragments.list
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.jd.todoapp.R
@@ -23,6 +21,14 @@ class ListFragment : Fragment() {
         view.listLayout.setOnClickListener {
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
+
+        //set menu
+
+        setHasOptionsMenu(true)
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 }
