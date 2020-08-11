f1648390ce64ffcd981a2daea1c9cea4bf76451a
jd <JUANDIEGOGARITA@users.noreply.github.com>
2020-08-10 20:36:01 -0600

    init basic view model class

---
+++ app/src/main/java/com/jd/todoapp/data/viewmodel/ToDoViewModel.kt
@@ -0,0 +1,32 @@
+package com.jd.todoapp.data.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.jd.todoapp.data.ToDoDatabase
+import com.jd.todoapp.data.models.ToDoData
+import com.jd.todoapp.data.repository.ToDoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ToDoViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val toDoDao = ToDoDatabase.getDataBase(
+        application
+    ).toDoDao()
+    private val repository: ToDoRepository
+
+    private val getAllData: LiveData<List<ToDoData>>
+
+    init {
+        repository = ToDoRepository(toDoDao)
+        getAllData = repository.getAllData
+    }
+
+    fun insertData(toDoData: ToDoData) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertData(toDoData)
+        }
+    }
+}
