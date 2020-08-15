28af02b3a611d73cfaac69063a3d6a250217de00
jd <JUANDIEGOGARITA@users.noreply.github.com>
2020-08-15 11:30:34 -0600

    moves the parse method from update fragment to shared view model

--- app/src/main/java/com/jd/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/jd/todoapp/fragments/update/UpdateFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.jd.todoapp.R
-import com.jd.todoapp.data.models.Priority
 import com.jd.todoapp.fragments.SharedViewModel
 import kotlinx.android.synthetic.main.fragment_update.view.*
 
@@ -24,20 +23,12 @@ class UpdateFragment : Fragment() {
 
         view.current_title_et.setText(args.currentItem.title)
         view.current_description_et.setText(args.currentItem.description)
-        view.current_priorities_spinner.setSelection(parsePriority(args.currentItem.priority))
+        view.current_priorities_spinner.setSelection(mSharedViewModel.parsePriorityToInt(args.currentItem.priority))
         view.current_priorities_spinner.onItemSelectedListener = mSharedViewModel.listener
         return view
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.update_fragment_menu, menu)
-    }
-
-    private fun parsePriority(priority: Priority): Int {
-        return when (priority) {
-            Priority.HIGH -> 0
-            Priority.MEDIUM -> 1
-            Priority.LOW -> 2
-        }
     }
 }
